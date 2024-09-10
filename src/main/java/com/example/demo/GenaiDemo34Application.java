18c40c3a145165a337bc7b4a7fc055bf820a6dd6
Sinajmon <you@example.comsinajmon.kalathiparambilaboo@deliveryoperations.onmicrosoft.com>
2024-09-10 10:52:19 +0000

    Updating Demo 42 details

---
+++ src/main/java/com/example/demo/GenaiDemo34Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenaiDemo34Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenaiDemo34Application.class, args);
+	}
+
+}
